55fc4120f5bc58b1ff70557644aed1f029bad004
zs <1145746534@qq.com>
2021-02-23 21:44:36 +0800

    123

---
+++ WinformMvvmTest/WinformMvvmTest/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿
+namespace WinformMvvmTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.button4 = new System.Windows.Forms.Button();
+            this.cartesianChart2 = new LiveCharts.WinForms.CartesianChart();
+            this.cartesianChart3 = new LiveCharts.WinForms.CartesianChart();
+            this.cartesianChart4 = new LiveCharts.WinForms.CartesianChart();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 27);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 25);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(189, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(280, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "改变变量值";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(189, 74);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(280, 25);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "改变文本框的值";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 74);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(29, 164);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 25);
+            this.textBox2.TabIndex = 3;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(189, 164);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(280, 25);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "实时变化";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Location = new System.Drawing.Point(29, 220);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(485, 192);
+            this.cartesianChart1.TabIndex = 7;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(534, 27);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(280, 25);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "图表增加一个点";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // cartesianChart2
+            // 
+            this.cartesianChart2.Location = new System.Drawing.Point(572, 220);
+            this.cartesianChart2.Name = "cartesianChart2";
+            this.cartesianChart2.Size = new System.Drawing.Size(485, 192);
+            this.cartesianChart2.TabIndex = 7;
+            this.cartesianChart2.Text = "cartesianChart1";
+            // 
+            // cartesianChart3
+            // 
+            this.cartesianChart3.Location = new System.Drawing.Point(29, 462);
+            this.cartesianChart3.Name = "cartesianChart3";
+            this.cartesianChart3.Size = new System.Drawing.Size(485, 192);
+            this.cartesianChart3.TabIndex = 7;
+            this.cartesianChart3.Text = "cartesianChart1";
+            // 
+            // cartesianChart4
+            // 
+            this.cartesianChart4.Location = new System.Drawing.Point(572, 462);
+            this.cartesianChart4.Name = "cartesianChart4";
+            this.cartesianChart4.Size = new System.Drawing.Size(485, 192);
+            this.cartesianChart4.TabIndex = 7;
+            this.cartesianChart4.Text = "cartesianChart1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(1182, 753);
+            this.Controls.Add(this.cartesianChart4);
+            this.Controls.Add(this.cartesianChart3);
+            this.Controls.Add(this.cartesianChart2);
+            this.Controls.Add(this.cartesianChart1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer1;
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private System.Windows.Forms.Button button4;
+        private LiveCharts.WinForms.CartesianChart cartesianChart2;
+        private LiveCharts.WinForms.CartesianChart cartesianChart3;
+        private LiveCharts.WinForms.CartesianChart cartesianChart4;
+    }
+}
+
---
+++ WinformMvvmTest/WinformMvvmTest/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinformMvvmTest
+{
+    public partial class Form1 : Form
+    {
+        TestClass test1 = new TestClass();
+        TestClass test2 = new TestClass();
+        List<LiveCharts.WinForms.CartesianChart> charts = new List<LiveCharts.WinForms.CartesianChart>();
+        int testnum = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            InitControllers();
+        }
+
+        /// <summary>
+        /// 控件逻辑的初始化
+        /// </summary>
+        private void InitControllers()
+        {
+            textBox1.DataBindings.Add("Text",test1, "QRCode",false,DataSourceUpdateMode.OnPropertyChanged);
+            textBox2.DataBindings.Add("Text",test1,"QRCode",false,DataSourceUpdateMode.OnPropertyChanged);
+
+            charts.Add(cartesianChart1);
+            charts.Add(cartesianChart2);
+            charts.Add(cartesianChart3);
+            charts.Add(cartesianChart4);
+
+            for (int i = 0; i < 4; i++)
+            {
+                if (i<2)
+                {
+                    charts[i].DataBindings.Add("AxisX", test1, "XAxis", false, DataSourceUpdateMode.OnPropertyChanged);
+                    charts[i].DataBindings.Add("Series", test1, "Series", false, DataSourceUpdateMode.OnPropertyChanged);
+                }
+                else
+                {
+                    charts[i].DataBindings.Add("AxisX", test2, "XAxis", false, DataSourceUpdateMode.OnPropertyChanged);
+                    charts[i].DataBindings.Add("Series", test2, "Series", false, DataSourceUpdateMode.OnPropertyChanged);
+                }
+                
+                charts[i].AxisX.Clear();
+                charts[i].AxisX.Add(new LiveCharts.Wpf.Axis());
+                charts[i].AxisX[0].Labels = new List<string>();
+            }
+
+            //cartesianChart1.DataBindings.Add("AxisX",test1, "XAxis",false,DataSourceUpdateMode.OnPropertyChanged);
+            //cartesianChart1.DataBindings.Add("Series",test1,"Series",false,DataSourceUpdateMode.OnPropertyChanged);
+            //cartesianChart1.AxisX.Clear();
+            //cartesianChart1.AxisX.Add(new LiveCharts.Wpf.Axis());
+            //cartesianChart1.AxisX[0].Labels = new List<string>();
+
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            test1.QRCode = new Random().Next(0,100).ToString();
+            label1.Text = "变量值" + test1.QRCode;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = new Random().Next(0, 100).ToString();
+            label1.Text = "变量值" + test1.QRCode;
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            test1.QRCode = new Random().Next(0,100).ToString();
+            label1.Text = "变量值" + test1.QRCode;
+            test1.XAxis[0].Labels.Add(testnum++.ToString());
+            test1.Series[0].Values.Add((double)testnum);
+
+            test2.XAxis[0].Labels.Add(testnum.ToString());
+            test2.Series[0].Values.Add((double)testnum);
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Enabled = false;
+            }
+            else
+            {
+                timer1.Enabled = true;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            test1.XAxis[0].Labels.Add(testnum++.ToString());
+            test1.Series[0].Values.Add((double)testnum);
+        }
+    }
+
+    class TestClass:INotifyPropertyChanged
+    {
+        string _QRCode;
+        LiveCharts.Wpf.AxesCollection _XAxis;
+        LiveCharts.SeriesCollection _series;
+
+        public LiveCharts.Wpf.AxesCollection XAxis {
+            get { return _XAxis; }
+            set {
+                _XAxis = value;
+                NotifyPropertyChanged("XAxis");
+            } }
+        public LiveCharts.SeriesCollection Series {
+            get { return _series; }
+            set 
+            {
+                _series = value;
+                NotifyPropertyChanged("Series");
+            }
+        }
+
+        public TestClass()
+        {
+            _XAxis = new LiveCharts.Wpf.AxesCollection();
+            _XAxis.Add(new LiveCharts.Wpf.Axis());
+            _XAxis[0].Labels = new List<string>();
+            _series = new LiveCharts.SeriesCollection();
+            _series.Add(new LiveCharts.Wpf.LineSeries() { Values = new LiveCharts.ChartValues<double>()});
+
+        }
+        public string QRCode {
+            get{ return _QRCode; }
+            set
+            {
+                _QRCode = value;
+                NotifyPropertyChanged("QRCode");
+            } }
+        
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string ProName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(ProName));
+            }
+        }
+    }
+    
+}
